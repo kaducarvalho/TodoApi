c5894cb3940cc161f46cddd6c08b1a99b80d7a1e
carloscarvalho3con <carlos.carvalho@trescon.com.br>
2026-03-12 18:49:25 -0300

    initial project test

---
+++ Controller/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello DevOps";
+    }
+}
